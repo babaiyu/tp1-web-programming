dbbe106a4140b795933bfdbc599469a27a389da9
Babaiyu <babaiyu@Babaiyus-MacBook-Pro.local>
2023-04-09 12:12:38 +0700

    Create auth (register, login, logout)

---
+++ README.md
@@ -0,0 +1,37 @@
+# TP2 - Autentikasi Laravel
+
+Tugas personal 2 yang dibuat dengan Autentikasi web pada Laravel
+
+### Prerequisite
+
+Run:
+
+```bash
+composer install
+cp .env.example .env
+touch database/database.sqlite
+php artisan key:generate
+php artisan jwt:secret
+```
+
+Edit `.env` file:
+
+```env
+DB_CONNECTION=sqlite
+# DB_CONNECTION=mysql
+# DB_HOST=127.0.0.1
+# DB_PORT=3306
+# DB_DATABASE=laravel
+# DB_USERNAME=root
+# DB_PASSWORD=
+
+...
+
+JWT_SHOW_BLACKLIST_EXCEPTION=true
+```
+
+Finally, run:
+
+```bash
+php artisan migrate:fresh --seed
+```
---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $loginInformation = $request->only('email', 'password');
+        $token = auth()->guard('api')->attempt($loginInformation);
+
+        if (!$token) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Wrong Email / Password! Please try again later'
+            ], 401);
+        }
+
+        return response()->json([
+            'success' => true,
+            'user' => auth()->guard('api')->user(),
+            'token' => $token,
+        ], 200);
+    }
+}
